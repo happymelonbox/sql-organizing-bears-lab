b0f6d951e4dcc1753be5041eabe52357277879c9
Justin Berger Howes <howes.j.j@gmail.com>
2021-09-05 08:57:03 +1000

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 54, "Male", "Brown", "Friendly", True),
+    ("Rowdy", 22, "Male", "Brown", "Friendly", True),
+    ("Tabitha", 3, "Female", "Black", "Friendly", True),
+    ("Sergeant Brown", 5, "Male", "Brown", "Friendly", True),
+    ("Melissa", 26, "Female", "Brown", "Friendly", True),
+    ("Grinch", 99, "Male", "Black", "Friendly", True),
+    ("Wendy", 43, "Female", "Brown", "Friendly", FALSE),
+    (NULL, NULL, "Male", "Brown", "Murderous", TRUE);
